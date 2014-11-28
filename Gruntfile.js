6d3755b71643645f418c8f92b15e1e674f26f7bc
sebeichholz <gene@oregami.org>
2014-11-28 23:40:59 +0100

    grunt process

--- Gruntfile.js
+++ Gruntfile.js
@@ -382,18 +382,18 @@ module.exports = function (grunt) {
   grunt.registerTask('build', [
     'clean:dist',
     'wiredep',
-    //'useminPrepare',
+    'useminPrepare',
     'concurrent:dist',
     'autoprefixer',
-    //'concat',
+    'concat',
     'ngAnnotate',
     'copy:dist',
     'cdnify',
-    //'cssmin',
+    'cssmin',
     //'uglify',
     'filerev',
-    //'usemin',
-    //'htmlmin'
+    'usemin',
+    'htmlmin'
   ]);
 
   grunt.registerTask('default', [
