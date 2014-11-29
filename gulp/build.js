b3cbf4dfbce792803995adbb8d3a2418c41869c5
sebeichholz <gene@oregami.org>
2014-11-29 22:39:21 +0100

    remove $.cache

--- gulp/build.js
+++ gulp/build.js
@@ -65,11 +65,15 @@ gulp.task('html', ['wiredep', 'scripts', 'partials'], function () {
 
 gulp.task('images', function () {
   return gulp.src('src/assets/images/**/*')
-    .pipe($.cache($.imagemin({
+    .pipe(
+    //$.cache(
+      $.imagemin({
       optimizationLevel: 3,
       progressive: true,
       interlaced: true
-    })))
+    })
+    //  )
+  )
     .pipe(gulp.dest('dist/assets/images'))
     .pipe($.size());
 });
